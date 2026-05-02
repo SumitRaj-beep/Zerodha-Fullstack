8cdd942dcd10598c5db398da34e37a19cef4ae06
Sumit Raj <sumitraj0857@gmail.com>
2026-05-02 16:07:25 +0530

    changed

--- backend/middleware.js
+++ backend/middleware.js
@@ -8,7 +8,7 @@ const verifyToken=(req,res,next)=>{
     try{
 
         const token=req.headers.authorization.split(" ")[1];
-        const  decoded=jwt.verify(token,"secretkey");
+        const  decoded=jwt.verify(token,process.env.JWT_SECRET);
         req.user=decoded;
    
         next();
